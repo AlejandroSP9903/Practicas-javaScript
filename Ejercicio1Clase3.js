a6d444339ff40eff9bb7461d7b0cd5ba4b067e6d
LuisAlejandroUmDev <alejo199905luis@gmail.com>
2025-11-20 21:48:25 -0500

    Create Ejercicio1Clase3.js

---
+++ Ejercicio1Clase3.js
@@ -0,0 +1,15 @@
+/*Pide al usuario cuántos nombres quiere ingresar.
+Luego, guarda cada nombre en un arreglo.
+Al final, imprime todos los nombres usando un ciclo.
+*/
+let cantidad = Number(propt('¿Cuantos nombres vas a ingresar?'));
+let nombres = [];
+for (let i = 1; i <= cantidad; i++) {
+  //Para solicitar una cadena de texto no es necesario poner "String" fuera de de los () y antes del prompt
+  // ya que js lo detectara como una cadena de texto o null
+  let nombre = prompt('Ingrese el nombre ' + i);
+  nombres.push(nombre);
+}
+for (let k = 0; k <= cantidad; k++){
+console.log(nombres[k]);
+}
